452dda04d8d14bc8a88a828a780a357982e19a72
Joonatan Renel <joonatanrenel@gmail.com>
2023-02-10 08:58:54 +0200

    Got to work with local machine...

--- DeviceLinkApp/DeviceLinkApp/Program.cs
+++ DeviceLinkApp/DeviceLinkApp/Program.cs
@@ -12,9 +12,19 @@ namespace DeviceLinkApp
     class Program
     {
         private const int DLPort = 1711;
+        private static string myIp;
 
         static void Main(string[] args)
         {
+            // Get the Name of HOST  
+            string hostName = Dns.GetHostName();
+            Console.WriteLine(hostName);
+
+            // Get the IP from GetHostByName method of dns class.
+            myIp = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            Console.WriteLine("IP Address is : " + myIp);
+
+
             BackgroundWorker senderWorker = new BackgroundWorker();
             senderWorker.DoWork += new DoWorkEventHandler(SenderThread);
             senderWorker.RunWorkerAsync();
@@ -29,20 +39,25 @@ namespace DeviceLinkApp
             UdpClient udpClient = new UdpClient();
             IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 1711);
 
-            udpClient.Client.ReceiveTimeout = 4000;
+            udpClient.Client.ReceiveTimeout = 1000;
 
             while (true)
             {
-                System.Threading.Thread.Sleep(2000);
+                System.Threading.Thread.Sleep(50);
 
                 Byte[] sendBytes1 = Encoding.ASCII.GetBytes("R/115");
                 Byte[] sendBytes2 = Encoding.ASCII.GetBytes("R/103");
                 Byte[] sendBytes3 = Encoding.ASCII.GetBytes("R/40");
                 try
                 {
-                    udpClient.Send(sendBytes1, sendBytes1.Length, "192.168.1.164", DLPort);
-                    udpClient.Send(sendBytes2, sendBytes2.Length, "192.168.1.164", DLPort);
-                    udpClient.Send(sendBytes3, sendBytes3.Length, "192.168.1.164", DLPort);
+                    //udpClient.Send(sendBytes1, sendBytes1.Length, "192.168.1.164", DLPort);
+                    //udpClient.Send(sendBytes2, sendBytes2.Length, "192.168.1.164", DLPort);
+                    //udpClient.Send(sendBytes3, sendBytes3.Length, "192.168.1.164", DLPort);
+
+                    //udpClient.Send(sendBytes1, sendBytes1.Length, myIp, DLPort);
+                    //udpClient.Send(sendBytes2, sendBytes2.Length, myIp, DLPort);
+                    udpClient.Send(sendBytes3, sendBytes3.Length, myIp, DLPort);
+
 
                 }
                 catch (Exception e)
